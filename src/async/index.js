636568196178d3deb181b8d47ae2fe1a51e5b8d8
Red_Robles <redworld22@hotmail.com>
2024-10-03 15:24:03 -0400

    Práctica y ejercicios de las funciones asíncronas

---
+++ src/async/index.js
@@ -0,0 +1,17 @@
+const asyncFn = ()=>{
+    return new Promise((resolve, reject)=>{
+        (true) ? setTimeout(() => {
+            resolve("Async")
+        }, 2000) : reject(new Error('Error'))
+    })
+}
+
+const anotherFn = async ()=>{
+    const something = await asyncFn();
+    console.log(something);
+    console.log('Hello');
+}
+
+console.log('Before');
+anotherFn()
+console.log('After');
